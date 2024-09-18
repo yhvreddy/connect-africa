e93f05f2d449bddf42bb45e78262dddc373a4e23
MustafaKhan026 <mohdmustafa17061@gmail.com>
2024-09-18 13:51:37 +0530

    removed routes

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,10 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Session;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ShowsController;
-use App\Http\Controllers\MoviesController;
-use App\Http\Controllers\EventsController;
 use App\Http\Controllers\AdminController;
-use App\Http\Controllers\PartnerController;
-use App\Http\Controllers\AffiliateController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DefaultController;
 use App\Http\Controllers\SettingsController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\GenresController;
-use App\Http\Controllers\EventTypeController;
-use App\Http\Controllers\OttController;
-use App\Http\Controllers\CategorizeController;
 use App\Http\Controllers\CountriesController;
 
 use App\Http\Controllers\CASubscriptionController;
@@ -49,70 +39,11 @@ Route::middleware(['zq'])->prefix('zq')->name('zq.')->group(function () {
 
     Route::get('admins/over-view/{admin}', [AdminController::class, 'adminOverview'])->name('admins.overview.details');
 
-
-    Route::resource('genres', GenresController::class);
-    Route::get('genres/soft-delete/{genre}', [GenresController::class, 'softDelete'])->name('genres.soft-delete');
-    Route::get('genre-ajax-data', [GenresController::class, 'genreFetchDataListForAjax'])->name('genres.fetch.data.ajax');
-    Route::put('/genres/{genreId}/{action}', [GenresController::class, 'updateStatus'])->name('genres.update.status');
-
     //Countries
     Route::resource('countries', CountriesController::class);
     Route::get('countries/soft-delete/{country}', [CountriesController::class, 'softDelete'])->name('countries.soft-delete');
     Route::get('countries-ajax-data', [CountriesController::class, 'fetchDataListForAjax'])->name('countries.fetch.data.ajax');
     Route::put('/countries/{country}/{action}', [CountriesController::class, 'updateStatus'])->name('countries.update.status');
-
-
-    //ott
-    Route::resource('ott', OttController::class);
-    Route::get('otts/soft-delete/{ott}', [OttController::class, 'softDelete'])->name('otts.soft-delete');
-    Route::get('ott-ajax-data', [OttController::class, 'ottFetchDataListForAjax'])->name('otts.fetch.data.ajax');
-    Route::put('/otts/{ottId}/{action}', [OttController::class, 'updateStatus'])->name('otts.update.status');
-
-    //Categorize
-    Route::resource('categorizes', CategorizeController::class);
-    Route::get('categorizes/soft-delete/{categorize}', [CategorizeController::class, 'softDelete'])->name('categorizes.soft-delete');
-    Route::get('categorize-ajax-data', [CategorizeController::class, 'fetchDataListForAjax'])->name('categorizes.fetch.data.ajax');
-    Route::put('/categorizes/{categorizeId}/{action}', [CategorizeController::class, 'updateStatus'])->name('categorizes.update.status');
-
-
-    //event-type
-    Route::resource('event-type', EventTypeController::class);
-    Route::get('event-types/soft-delete/{event-type}', [EventTypeController::class, 'softDelete'])->name('event-types.soft-delete');
-    Route::get('event-type-ajax-data', [EventTypeController::class, 'eventTypeFetchDataListForAjax'])->name('event-type.fetch.data.ajax');
-    Route::put('/event-type/{eventTypeId}/{action}', [EventTypeController::class, 'updateStatus'])->name('event-type.update.status');
-
-    Route::resource('partners', PartnerController::class);
-    Route::get('deactivated/partners-list', [PartnerController::class, 'deactivatedList'])
-        ->name('partners.deactivated.list');
-    Route::get('partners/soft-delete/{partner}', [PartnerController::class, 'softDelete'])->name('partners.soft-delete');
-    Route::get('partner-ajax-data', [PartnerController::class, 'partnerFetchDataListForAjax'])->name('partner.fetch.data.ajax');
-    Route::put('/partners/{partnerId}/{action}', [PartnerController::class, 'updateStatus'])->name('partner.update.status');
-
-    //payment
-    Route::prefix('payments')->name('payments.')->group(function () {
-        Route::get('/', [PaymentController::class, 'index'])->name('index');
-        Route::get('payments/load-data-ajax', [PaymentController::class, 'fetchTransactionsList'])->name('fetch.data.ajax');
-    });
-
-    //Affiliates routes
-    Route::prefix('affiliates')->name('affiliates.')->group(function () {
-        Route::get('/', [AffiliateController::class, 'index'])->name('index');
-        Route::get('/create', [AffiliateController::class, 'create'])->name('create');
-        Route::post('/', [AffiliateController::class, 'store'])->name('store');
-        Route::get('/{affiliate}/edit', [AffiliateController::class, 'edit'])->name('edit');
-        Route::put('/{affiliate}', [AffiliateController::class, 'update'])->name('update');
-        Route::get('/show/{affiliate}', [AffiliateController::class, 'show'])->name('show');
-        Route::get('/show/users/{affiliate}/{type}', [AffiliateController::class, 'showUserDetails'])->name('show.users');
-
-        Route::get('deactivated-list', [AffiliateController::class, 'deactivatedList'])
-            ->name('deactivated.list');
-
-
-        Route::get('soft-delete/{affiliate}', [AffiliateController::class, 'softDelete'])->name('soft-delete');
-        Route::get('ajax-data', [AffiliateController::class, 'fetchDataListForAjax'])->name('fetch.data.ajax');
-        Route::get('users/ajax-data', [AffiliateController::class, 'fetchAffiliateUsersListForAjax'])->name('users.fetch.data.ajax');
-        Route::put('/affiliate/{affiliateId}/{action}', [AffiliateController::class, 'updateStatus'])->name('update.status');
-    });
 
     Route::get('profile/{zq}', [IndexController::class, 'profileEdit'])->name('profile.edit');
     Route::put('profile/{zq}', [IndexController::class, 'profileUpdate'])->name('profile.update');
